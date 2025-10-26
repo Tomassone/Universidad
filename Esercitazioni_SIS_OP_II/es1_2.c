b4f1a6489a10535fff81ce930218b8d62500e823
Tomassone <86415106+Tomassone@users.noreply.github.com>
2025-10-26 19:04:45 +0100

    ...

--- Esercitazioni_SIS_OP_II/es1_2.c
+++ Esercitazioni_SIS_OP_II/es1_2.c
@@ -1,10 +1,13 @@
-#include stdio.h
-#include stdlib.h
-#include pthread.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 #define N 10
 #define K 5
 
 #define s_mutex s.sem_mutex
+
+// gcc -o es1_2 es1_2.c
 
 typedef struct {
     char film[K][40];
@@ -15,60 +18,61 @@ typedef struct {
 
 sondaggio s;
 
-void thread_process (void arg)
+void *thread_process (void * arg)
 {
-    int temp;
-    long id;
-    id = (long) arg;
+    int temp = 0;
+    pthread_mutex_lock(&(s.sem_mutex)); 
 
-    pthread_mutex_lock(&s.sem_mutex); prologo
+    s.pareri++;
 
-    s.film[id][0] = 'I';
-    s.pareri[id] = 'i';
-    s.film[id][1] = 'M';
-    s.pareri[id] = 'm';
-    s.film[id][2] = 'A';
-    s.pareri[id] = 'a';
-
-    for (int i = 0; i  K; i++) {
+    for (int i = 0; i < K; i++) {
         temp = rand() % 10 + 1;
         s.voti[i] = s.voti[i] + temp;
-        printf(voto %dt%st%dn, temp, s.film[i], s.voti[i]);
+        printf("Valore generato: %d, %d, %d \n", temp, i, s.voti[i]);
     }
 
-    printf(thread %ld dice %s, pareri %cn, id, s.pareri[id]);
-    pthread_mutex_unlock(&s.sem_mutex); epilogo
+    printf("Accessi: %d\n", s.pareri);
+    pthread_mutex_unlock(&(s.sem_mutex));
     pthread_exit(0);
 }
 
 int main()
 {
-    Dichiarazioni...
+    // dichiarazioni
     pthread_t utenti[N];
+    srand(time(0));
 
-    Inizializzazione struttura dati globale
-    for (int i = 0; i  K; i++) {
-        s.pareri[i] = 0;
+    // inizializzazione struttura dati globale
+    s.pareri = 0;
+    for (int i = 0; i < K; i++) {
         s.voti[i] = 0;
+
+        s.film[i][0] = 'F';
+        s.film[i][1] = 'I';
+        s.film[i][2] = 'L';
+        s.film[i][3] = 'M';
+        s.film[i][4] = i + 48 + 1;
+        s.film[i][5] = '\0';
     }
 
-    pthread_mutex_init(&s.sem_mutex, NULL);
+    pthread_mutex_init(&(s.sem_mutex), NULL);
 
-    for (long i = 0; i  N; i++) {
-        if (pthread_create(&utenti[i], NULL, thread_process, (void ) i)  0) {
-            fprintf(stderr, create error for thread %ldn, i);
+    for (int i = 0; i < N; i++) {
+        if (pthread_create(&(utenti[i]), NULL, thread_process, NULL) < 0) {
+            fprintf(stderr, "create error for thread %d\n", i);
             exit(1);
         }
     }
 
-    Attesa terminazione thread
-    for (int i = 0; i  N; i++) {
+    // attesa terminazione thread
+    for (int i = 0; i < N; i++) {
         pthread_join(utenti[i], NULL);
     }
 
-    for (int i = 0; i  K; i++) {
-        printf(film %stvoti %dn, s.film[i], s.voti[i]);
+    for (int i = 0; i < K; i++) {
+        printf("\n%s %d", s.film[i], s.voti[i] / N);
     }
+    printf("\n");
 
     return 0;
 }
--- Esercitazioni_SIS_OP_II/es2_2.c
+++ Esercitazioni_SIS_OP_II/es2_2.c
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <semaphore.h>
 
-#define MaxP 5
-#define MaxA 8
-#define NumGruppi 30
+#define MaxP 8
+#define MaxA 12
+#define NumGruppi 20
 
 // gcc -o es2_2 es2_2.c
 
@@ -21,33 +21,33 @@ typedef struct{
 
 parco p;
 
+void shift_array(int* array, int index)
+{
+    for (int i = index; i < NumGruppi - 1; i++)
+        array[i] = array[i + 1];
+    array[NumGruppi - 1] = 0;
+}
+
 void entrata(int n_visitatori, int index)
 {
     pthread_mutex_lock(&(p.m));
 
-    while (n_visitatori > p.posti_liberi || (p.auto_libere > 0) || 
-        !gruppi_sospesi[ordine_arrivo])
+    while (n_visitatori > p.posti_liberi || (p.auto_libere <= 0) || 
+        (p.ordine_arrivo > 0 && p.gruppi_sospesi[p.ordine_arrivo - 1]))
     { 
-        p.sospesi++;
+        p.gruppi_sospesi[p.ordine_arrivo] = 1;
+        p.ordine_arrivo++;
         pthread_mutex_unlock(&(p.m));
         printf("Processo %d sospeso all'entrata!\n", index);
         sem_wait(&p.S);
         pthread_mutex_lock(&(p.m));
-        p.sospesi--;
+        p.gruppi_sospesi[p.ordine_arrivo - 1] = 0;
         printf("Processo %d attivato all'entrata!\n", index);
     }
     
-    p.posti_liberi--;
-    if (tipo == MONOPATTINO)
-    {
-        printf("Processo %d ha preso un monopattino!\n", index);
-        p.monop_liberi--;
-    }
-    else
-    {
-        printf("Processo %d ha preso una bici!\n", index);
-        p.bici_libere--;
-    }
+    p.posti_liberi -= n_visitatori;
+    p.auto_libere--;
+    printf("Processo %d ha preso una macchina!\n", index);
 
     pthread_mutex_unlock(&(p.m));
 }
@@ -56,12 +56,14 @@ void uscita(int n_visitatori, int index)
 {
     pthread_mutex_lock(&(p.m));
     
-    p.posti_liberi++;
+    p.posti_liberi += n_visitatori;
     p.auto_libere++;
+    shift_array(p.gruppi_sospesi, p.ordine_arrivo);
+    p.ordine_arrivo--;
 
     pthread_mutex_unlock(&(p.m));
 
-    for (int i = 0 ; i < p.sospesi; i++)
+    for (int i = 0 ; i < NumGruppi; i++)
         sem_post(&p.S);
 
     printf("Processo %d uscito!\n", index);
@@ -79,7 +81,7 @@ void *thread_process (void * arg)
     uscita(n_visitatori, indice);
 
     printf("Processo %d ha terminato!\n", indice);
-    pthread_exit (0);
+    pthread_exit(0);
 }
 
 int main()
@@ -91,6 +93,8 @@ int main()
     p.auto_libere = MaxA;
     p.posti_liberi = MaxP;
     p.ordine_arrivo = 0;
+    for (int i = 0; i < NumGruppi; i++)
+        p.gruppi_sospesi[i] = 0;
 
     // i semafori sono inizialmente liberi:
     pthread_mutex_init (&(p.m), NULL);
