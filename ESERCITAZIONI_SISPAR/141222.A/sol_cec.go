75356af857507a02a09208a44ab34c089943a45a
Tomassone <86415106+Tomassone@users.noreply.github.com>
2025-12-26 15:16:01 +0100

    Update sol_cec.go

--- ESERCITAZIONI_SISPAR/141222.A/sol_cec.go
+++ ESERCITAZIONI_SISPAR/141222.A/sol_cec.go
@@ -6,31 +6,23 @@ import (
 	"time"
 )
 
-const maxSpettatatori = 15 //max spettatori nello stadio
-const maxControllori = 3 //max controllori al varco
-var maxLocali = 0
-var maxOspiti = 0
+const maxClienti = 15 
+const maxFrigorifero = 5
+const maxTortellini = 10
+const maxSfogline = 7
 
-const tipoLocale int = 0
-const tipoOspite int = 1
+var done = make(chan bool)fz
+var terminaLaboratorio = make(chan bool)
 
-var tipoSpettatore = [2]string{"Locale", "Ospite"}
-
-var done = make(chan bool)
-var terminaBiglietteria = make(chan bool)
-var terminaVarco = make(chan bool)
-
-//canali usati dagli spettatori per l'acquisto dei biglietti
-var acquistoLocali = make(chan int, 100)
-var acquistoOspiti = make(chan int, 100)
-
-//canali usati dagli spettatori per il controllo della sicurezza
-var controlloLocali = make(chan int, 100)
-var controlloOspiti = make(chan int, 100)
+var richiestePrenotazioni = make(chan int, 100)
+var richiesteRitiro = make(chan int, 100)
+var richiesteDeposito = make(chan int, 100)
 
 //canali di ack
-var ack_locali = make(chan int)   // canale di ack per il robot relativo ai locali
-var ack_ospiti = make(chan int)   // canale di ack per il robot relativo agli ospiti
+var ack_clienti = make(chan int) 
+var ack_sfogline = make(chan int)
+
+var idCounter int = 0
 
 func when(b bool, c chan int) chan int {
 	if !b {
@@ -39,159 +31,132 @@ func when(b bool, c chan int) chan int {
 	return c
 }
 
-func Spettatore(tipo int) {
-	var tt int
-	fmt.Printf("[Spettatore %s]: partenza! \n", tipoSpettatore[tipo])
+func Cliente() {
+	var id int
+	fmt.Printf("[Cliente]: partenza! \n")
 	var ris int
 
-	if tipo == 0 {
-		acquistoLocali <- 1
-		ris = <-ack_locali
-		if ris == -1 {
-			fmt.Printf("[Spettatore %s]: termino! \n", tipoSpettatore[tipo])
-			done <- true
-			return
-		}
-		tt = (rand.Intn(2) + 1) //tempo di attesa biglietto
-		time.Sleep(time.Duration(tt) * time.Second)
-		fmt.Printf("[Spettatore %s]: preso biglietto! \n", tipoSpettatore[tipo])
-
-		controlloLocali <- 1
-		ris = <-ack_locali
-		if ris == -1 {
-			fmt.Printf("[Spettatore %s]: termino! \n", tipoSpettatore[tipo])
-			done <- true
-			return
-		}
-		fmt.Printf("[Spettatore %s]: effettuato controllo! \n", tipoSpettatore[tipo])
-	} else {
-		acquistoOspiti <- 1
-		ris = <-ack_ospiti
-		if ris == -1 {
-			fmt.Printf("[Spettatore %s]: termino! \n", tipoSpettatore[tipo])
-			done <- true
-			return
-		}
-		tt = (rand.Intn(2) + 1)
-		time.Sleep(time.Duration(tt) * time.Second) //tempo di attesa controllo
-		fmt.Printf("[Spettatore %s]: preso biglietto! \n", tipoSpettatore[tipo])
-
-		controlloOspiti <- 1
-		ris = <-ack_ospiti
-		if ris == -1 {
-			fmt.Printf("[Spettatore %s]: termino! \n", tipoSpettatore[tipo])
-			done <- true
-			return
-		}
-		fmt.Printf("[Spettatore %s]: effettuato controllo! \n", tipoSpettatore[tipo])
+	richiestePrenotazioni <- 1
+	ris = <-ack_clienti
+	id = ris //ottengo in risposta l'id della richiesta effettuata
+	if ris == -1 {
+		fmt.Printf("[Cliente]: termino! \n")
+		done <- true
+		return
 	}
-	fmt.Printf("[Spettatore %s]: è entrato!\n", tipoSpettatore[tipo])
+	fmt.Printf("[Cliente %d]: effettuata prenotazione! \n", id)
+
+	richiesteRitiro <- id
+	ris = <-ack_clienti
+	if ris == -1 {
+		fmt.Printf("[Cliente]: termino! \n")
+		done <- true
+		return
+	}
+	fmt.Printf("[Cliente %d]: ritirata prenotazione! \n", id)
+
 	done <- true
 }
 
-func biglietteria() {
-	var fine bool = false // diventa true quando sono stati dati tutti i biglietti
-	var bigliettiDati int = 0
+func Sfoglina() {
+	fmt.Printf("[Sfoglina]: partenza! \n")
+	var ris int
+
+	richiesteDeposito <- 1
+	ris = <-ack_sfogline
+	if ris == -1 {
+		fmt.Printf("[Sfoglina]: termino! \n")
+		done <- true
+		return
+	}
+	fmt.Printf("[Sfoglina]: effettuato deposito! \n")
+
+	done <- true
+}
+
+func gestioneLaboratorio() {
+	var ritiriEffettuati int = 0, depositiEffettuati int = 0;
+	var tortelliniDisponibili int = 0;
+	var isIdUsed [maxClienti]bool;
+	var fineClienti bool = false, fineSpogline bool = false;
+	var idIndex int = 0, tt int = 0;
 
 	for {
 		select {
-			case <-when(fine == false && bigliettiDati < maxSpettatatori, acquistoLocali):
-				bigliettiDati++
-				ack_locali <- 1
-				fmt.Printf("[biglietti]: locale entrato in biglietteria, ci sono %d spettatori con biglietto\n", bigliettiDati)
-			case <-when(fine == false && bigliettiDati < maxSpettatatori, acquistoOspiti):
-				bigliettiDati++
-				ack_ospiti <- 1
-				fmt.Printf("[biglietti]: ospite entrato in biglietteria, ci sono %d spettatori con biglietto\n", bigliettiDati)
+			case <-when(fineSpogline == false && depositiEffettuati < maxSfogline && tortelliniDisponibili < maxFrigorifero && len(richiestePrenotazioni) == 0 && len(richiesteRitiro) == 0, richiesteDeposito):
+				depositiEffettuati++
+				tortelliniDisponibili++
+				tt = (rand.Intn(2) + 1)
+				time.Sleep(time.Duration(tt) * time.Second)
+				ack_sfogline <- 1
+				fmt.Printf("[Gestione laboratorio]: aggiunto tortellino, ci sono %d tortellini dispoinibili e %d ritiri effettutati; tot depositi effettuati = %d\n", tortelliniDisponibili, ritiriEffettuati, depositiEffettuati)
+			case <-when(fineClienti == false && ritiriEffettuati < maxClienti && tortelliniDisponibili > 0, richiestePrenotazioni):
+				tt = (rand.Intn(2) + 1)
+				time.Sleep(time.Duration(tt) * time.Second)
+				ack_clienti <- idCounter
+				idCounter++
+				fmt.Printf("[Gestione laboratorio]: prenotato tortellino, ci sono %d tortellini dispoinibili e %d ritiri effettutati; tot depositi effettuati = %d\n", tortelliniDisponibili, ritiriEffettuati, depositiEffettuati)
+			case <-when(fineClienti == false && ritiriEffettuati < maxClienti && tortelliniDisponibili > 0 && len(richiestePrenotazioni) == 0, idIndex <- richiesteRitiro):
+				tortelliniDisponibili--
+				ritiriEffettuati++
+				tt = (rand.Intn(2) + 1)
+				time.Sleep(time.Duration(tt) * time.Second)
+				if isIdUsed[idIndex] == false {
+					ack_clienti <- 1
+					isIdUsed[idIndex] = true	
+				}
+				else {
+					ack_clienti <- -1
+				}
+				fmt.Printf("[Gestione laboratorio]: prenotato tortellino, ci sono %d tortellini dispoinibili e %d ritiri effettutati; tot depositi effettuati = %d\n", tortelliniDisponibili, ritiriEffettuati, depositiEffettuati)
 
 			//terminazione
-			case <-when(fine == true, acquistoOspiti):
-				ack_ospiti <- -1
-			case <-when(fine == true, acquistoLocali):
-				ack_locali <- -1
+			case <-when(fineSfogline == true, richiesteDeposito):
+				ack_sfogline <- -1
+			case <-when(fineClienti == true, richiestePrenotazioni):
+				ack_clienti <- -1
+			case <-when(fineClienti == true, richiesteRitiro):
+				ack_clienti <- -1
 			
-			case <-terminaBiglietteria:
-				fmt.Printf("[biglietteria]: termino\n")
+			case <-terminaLaboratorio:
+				fmt.Printf("[Gestione Laboratorio]: termino\n")
 				done <- true
 				return
 		}
 
-		if bigliettiDati == maxSpettatatori {
-			fine = true
-		}
-	}
-}
-
-func controlloVarco() {
-	var numOspitiEntrati int = 0;
-	var numLocaliEntrati int = 0;
-	var controlloriLiberi int = maxControllori;
-	var fine bool = false // diventa true quando sono stati completati i controlli
-	var tt int = 0;
-
-	for {
-		select {
-		case <-when(fine == false && (numOspitiEntrati < maxOspiti) && (controlloriLiberi > 0) &&
-			((numLocaliEntrati <= numOspitiEntrati) || (numLocaliEntrati > numOspitiEntrati && (numLocaliEntrati == maxLocali || len(controlloOspiti) == 0))), controlloOspiti):
-			controlloriLiberi--
-			tt = (rand.Intn(2) + 1)
-			time.Sleep(time.Duration(tt) * time.Second) //tempo di attesa controllo
-			controlloriLiberi++
-			numOspitiEntrati++
-			ack_ospiti <- 1
-			fmt.Printf("[controllo varco]: controllato ospite, ci sono %d ospiti e %d locali entrati; tot controllori liberi = %d\n", numOspitiEntrati, numLocaliEntrati, controlloriLiberi)
-		case <-when(fine == false && (numLocaliEntrati < maxLocali) && (controlloriLiberi > 0) &&
-			((numOspitiEntrati <= numLocaliEntrati) || (numOspitiEntrati > numLocaliEntrati && (numOspitiEntrati == maxOspiti || len(controlloLocali) == 0))), controlloLocali):
-			controlloriLiberi--
-			tt = (rand.Intn(2) + 1)
-			time.Sleep(time.Duration(tt) * time.Second) //tempo di attesa controllo
-			controlloriLiberi++
-			numLocaliEntrati++
-			ack_locali <- 1
-			fmt.Printf("[controllo varco]: controllato locale, ci sono %d ospiti e %d locali entrati; tot controllori liberi = %d\n", numOspitiEntrati, numLocaliEntrati, controlloriLiberi)
-
-		//terminazione
-		case <-when(fine == true, controlloOspiti):
-			ack_ospiti <- -1
-		case <-when(fine == true, controlloLocali):
-			ack_locali <- -1
-		
-		case <-terminaVarco:
-			fmt.Printf("[varco]: termino\n")
-			done <- true
-			return
+		if ritiriEffettuati == maxClienti {
+			fineClienti = true
 		}
 
-		if numOspitiEntrati + numLocaliEntrati == maxSpettatatori {
-			fine = true
+		if depositiEffettuati == maxSfogline {
+			fineSfogline = true
 		}
 	}
 }
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	maxLocali = rand.Intn(maxSpettatatori) + 1
-	maxOspiti = maxSpettatatori - maxLocali
 	fmt.Printf("[main] attivo programma\n")
 
-	go biglietteria()
-	go controlloVarco()
+	go gestioneLaboratorio()
 
-	for i := 0; i < maxLocali; i++ {
-		go Spettatore(0)
+	for i := 0; i < maxSfogline; i++ {
+		go Sfoglina()
 	}
 
-	for i := 0; i < maxOspiti; i++ {
-		go Spettatore(1)
+	for i := 0; i < maxClienti; i++ {
+		go Cliente()
 	}
 
-	for i := 0; i < maxSpettatatori; i++ { //terminazione spettatori
+	for i := 0; i < maxSfogline; i++ { 
 		<-done
 	}
 
-	terminaVarco <- true
-	<-done
-	terminaBiglietteria <- true
+	for i := 0; i < maxClienti; i++ { 
+		<-done
+	}
+
+	terminaLaboratorio <- true
 	<-done
 	fmt.Printf("[main] APPLICAZIONE TERMINATA \n")
 }
